df5dc018d64932427e0db19f0221300fa8911d3a
ekateryna <shtepazp87@gmail.com>
2025-03-07 09:08:44 +0300

    Homowork5 (виправлення в task3.js

--- homework5/task3.js
+++ homework5/task3.js
@@ -1,7 +1,15 @@
 let num = 6
 let count = 1;
 while (count <= 10) {
-    console.log(num, ' x ', count, ' = ', num*count)
+  let result = num*count
+  console.log(`${ num }  x  ${count} =  ${result}`)
     count++;
 }
 
+
+let numb = 3
+let counter = 1
+for (; counter < 11; counter++) {
+  let resulter = numb*counter
+console.log(`${ numb }  x  ${counter} =  ${resulter}`)
+}
