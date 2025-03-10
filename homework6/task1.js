f9eb6a0d68434a106d4d3f652f96ab7f2690ebdc
ekateryna <shtepazp87@gmail.com>
2025-03-10 17:33:01 +0300

    Homowork6

---
+++ homework6/task1.js
@@ -0,0 +1,18 @@
+//1. declaration
+function area_rectangle(width, height) {
+    
+  return info = width*height;
+}
+console.log(area_rectangle(5, 10));
+
+//2. expression
+const area_rectangle2=function (width, height) {
+    
+  return info = width*height;
+}
+console.log(area_rectangle2(3, 10));
+
+//3. arrow function
+const area_rectangle3 = (width, height) => width * height
+
+console.log(area_rectangle3(2, 3));
